b45726324973f53bc354f43f3810c1ffe631d044
DimaLitvinenko <80059894+DimaLitvinenko@users.noreply.github.com>
2021-09-12 22:35:32 +0300

    update task

--- src/app.js
+++ src/app.js
@@ -64,57 +64,121 @@ const galleryItems = [
   },
 ];
 
-
-const listRef = document.querySelector('.js-gallery');
-
-// // Создание и рендер для строк
-// const createGalleryCard = (items) => {
-//   return listRef.insertAdjacentHTML(
-//     'beforeend',
-//     items.map(({ preview, original, description }) => {
-//       return `
-//       <li class="gallery__item">
-//         <a
-//           class="gallery__link"
-//           href="${original}"
-//         >
-//           <img
-//             class="gallery__image"
-//             src="${preview}"
-//             data-source="${original}"
-//             alt="${description}"
-//           />
-//         </a>
-//       </li>`
-//     })
-//       .join('')
-//   );
-// };
-// const markup = createGalleryCard(galleryItems);
-
-
-// Создание и рендер для обьекта
-const createGalleryCard = ({ preview, original, description }) => {
-  const itemEl = document.createElement('li');
-  itemEl.classList.add('gallery__item');
-
-  const linkEl = document.createElement('a');
-  linkEl.classList.add('gallery__link');
-  linkEl.href = `${original}`;
-
-  const imageEl = document.createElement('img');
-  imageEl.classList.add('gallery__image');
-  imageEl.src = `${preview}`;
-  imageEl.dataset.source = `${original}`;
-  imageEl.alt = `${description}`;
-
-  linkEl.append(imageEl);
-  itemEl.append(linkEl);
-  
-  return itemEl;
+const refs = {
+  list: document.querySelector('.js-gallery'),
+  container: document.querySelector('.lightbox'),
+  image: document.querySelector('.lightbox__image'),
+  closeBtn: document.querySelector('[data-action="close-lightbox"]'),
+  overlay: document.querySelector('.lightbox__overlay'),
 };
 
-const markupElements = galleryItems.map(createGalleryCard);
-console.log(markupElements);
+const { list, container, image, closeBtn, overlay } = refs;
 
-listRef.append(...markupElements);
+// - Создание и рендеринг разметки для строки
+const createGalleryCard = items => {
+  return list.insertAdjacentHTML(
+    'beforeend',
+    items.map(({ preview, original, description }) => {
+      return `
+      <li class="gallery__item">
+        <a
+          class="gallery__link"
+          href="${original}"
+        >
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </li>`
+    })
+      .join('')
+  );
+};
+
+const markup = createGalleryCard(galleryItems);  // вызов функции 
+
+
+// - Создание и рендеринг разметки для обьекта
+// const createGalleryCard = ({ preview, original, description }) => {
+//   const itemEl = document.createElement('li');
+//   itemEl.classList.add('gallery__item');
+
+//   const linkEl = document.createElement('a');
+//   linkEl.classList.add('gallery__link');
+//   linkEl.href = `${original}`;
+
+//   const imageEl = document.createElement('img');
+//   imageEl.classList.add('gallery__image');
+//   imageEl.src = `${preview}`;
+//   imageEl.dataset.source = `${original}`;
+//   imageEl.alt = `${description}`;
+
+//   linkEl.append(imageEl);
+//   itemEl.append(linkEl);
+  
+//   return itemEl;
+// };
+// const markupElements = galleryItems.map(createGalleryCard);
+// console.log(markupElements);
+// list.append(...markupElements);
+
+
+// - Реализация делегирования на галерее `ul.js-gallery` и получение `url` большого
+//   изображения.
+// - Открытие модального окна по клику на элементе галереи.
+// - Подмена значения атрибута `src` элемента `img.lightbox__image`.
+const itemGalleryHandler = event => {
+  console.log(event.target);
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  };
+
+  event.preventDefault();
+
+  container.classList.add('is-open');
+  image.src = event.target.dataset.source;
+  image.alt = event.target.alt;
+  console.log(image.alt);
+};
+
+// - Закрытие модального окна по клику на кнопку
+//   `button[data-action="close-lightbox"]`.
+// - Очистка значения атрибута `src` элемента `img.lightbox__image`. Это необходимо
+//   для того, чтобы при следующем открытии модального окна, пока грузится
+//   изображение, мы не видели предыдущее.
+const modalCloseHandler = () => {
+  image.src = "";
+  image.alt = "";
+  // console.log(image.alt);
+  container.classList.remove('is-open');
+};
+
+// - Закрытие модального окна по нажатию клавиши `ESC`.
+const modalCloseByEscHandler = event => {
+  console.log(event.key);
+  // console.log(event.code);
+  if (event.key === 'Escape') {
+    modalCloseHandler();
+  };
+};
+
+// - Закрытие модального окна по клику на `div.lightbox__overlay`.
+const modalCloseByOverlayHandler = event => {
+  console.log(event.target);
+  modalCloseHandler();
+};
+
+list.addEventListener('click', itemGalleryHandler);
+
+closeBtn.addEventListener('click', modalCloseHandler);
+
+window.addEventListener('keyup', modalCloseByEscHandler);
+
+overlay.addEventListener('click', modalCloseByOverlayHandler)
+
+
+// - Пролистывание изображений галереи в открытом модальном окне клавишами "влево"
+//   и "вправо".
