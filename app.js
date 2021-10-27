5decdf845c4b260e20b24595074092a7c8fba8a2
dimon4 <80059894+DimaLitvinenko@users.noreply.github.com>
2021-10-27 16:57:27 +0200

    update

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 import galleryItems from "./references/gallery.js";
-
 const refs = {
   list: document.querySelector('.js-gallery'),
   container: document.querySelector('.js-lightbox'),
@@ -7,36 +6,9 @@ const refs = {
   closeBtn: document.querySelector('[data-action="close-lightbox"]'),
   overlay: document.querySelector('.lightbox__overlay'),
 };
-
 const { list, container, image, closeBtn, overlay } = refs;
 
-// ВАРИАНТ 1- Создание и рендеринг разметки для обьекта
-// const createGalleryCard = ({ preview, original, description }) => {
-//   const itemEl = document.createElement('li');
-//   itemEl.classList.add('gallery__item');
-
-//   const linkEl = document.createElement('a');
-//   linkEl.classList.add('gallery__link');
-//   linkEl.href = `${original}`;
-
-//   const imageEl = document.createElement('img');
-//   imageEl.classList.add('gallery__image');
-//   imageEl.src = `${preview}`;
-//   imageEl.dataset.source = `${original}`;
-//   imageEl.alt = `${description}`;
-
-//   linkEl.append(imageEl);
-//   itemEl.append(linkEl);
-  
-//   return itemEl;
-// };
-// const markupElements = galleryItems.map(createGalleryCard);
-// console.log(markupElements);
-// list.append(...markupElements);
-
-
-// ВАРИАНТ 2- Создание и рендеринг разметки для строки
-const createGalleryCard = items => {
+const createGalleryCard = items => {               // Создание и рендеринг разметки для строки
   return list.insertAdjacentHTML(
     'beforeend',
     items.map(({ preview, original, description }, index) => {
@@ -59,114 +31,63 @@ const createGalleryCard = items => {
       .join('')
   );
 };
+const markup = createGalleryCard(galleryItems, galleryItems.length);  
 
-const markup = createGalleryCard(galleryItems, galleryItems.length);  // вызов функции 
-
-// - Закрытие модального окна по нажатию клавиши `ESC`.
-const itemGalleryEscHandler = ({ key }) => {
+const itemGalleryEscHandler = ({ key }) => {                      // - Закрытие модального окна по нажатию клавиши `ESC`.
   if (key === 'Escape') {
-    itemGalleryCloseHandler();
+    itemGalleryCloseHandler();    
   };
-
-  console.log(key);
 };
 
-// - Пролистывание изображений галереи в открытом модальном окне клавишами "влево"
-//   и "вправо".
-const scrollGalleryHandler = ({ key }) => {
-  // let activeIndex = null;
-  // markup.forEach((el, ind) => {
-  //   if (el.includes(e.target.src)) {
-  //     activeIndex = ind;
-  //     return;
-  //   }
-  // });
-  
-  // if (key === "ArrowRight" && galleryItems.length - 1 > activeIndex) {
-  //   activeIndex += 1;
-  //   refs.imgInModal.src = galleryItems[activeIndex].original;
-  //   return;
-  // }
-  // if (key === "ArrowLeft" && activeIndex > 0) {
-  //   activeIndex -= 1;
-  //   refs.imgInModal.src = galleryItems[activeIndex].original;
-  //   return;
-  // }
-  // if (key === "ArrowRight" && activeIndex === galleryItems.length - 1) {
-  //   activeIndex = 0;
-  //   refs.imgInModal.src = galleryItems[activeIndex].original;
-  //   return;
-  // }
-  // if (key === "ArrowLeft" && activeIndex === 0) {
-  //   activeIndex = galleryItems.length - 1;
-  //   refs.imgInModal.src = galleryItems[activeIndex].original;
-  //   return;
-  // }
+const scrollGalleryHandler = ({ key }) => {   
   let currentIndex = galleryItems.findIndex(
-    item =>
-      item.description === image.alt
-      || item.original === image.src
+    item => item.description === image.alt || item.original === image.src
   );
-  
-  if (key === "ArrowLeft") {
+  if (key === "ArrowLeft") {                // - Пролистывание изображений галереи в открытом модальном окне стрелками "влево".
     if (currentIndex !== 0) {
       currentIndex -= 1;
     } else {
       currentIndex = 0;
     };
   };
-
-  if (key === "ArrowRight") {
+  if (key === "ArrowRight") {                // - Пролистывание изображений галереи в открытом модальном окне стрелками "вправо".
     if (currentIndex !== galleryItems.length - 1) {
       currentIndex += 1;
     } else {
       currentIndex = galleryItems.length - 1;
     };
-  };
-
-  console.log(currentIndex);
+  }; 
   
   image.alt = galleryItems[currentIndex].description;
   image.src = galleryItems[currentIndex].original;
 };
 
-// - Реализация делегирования на галерее `ul.js-gallery` и получение `url` большого
-//   изображения.
-// - Открытие модального окна по клику на элементе галереи.
-// - Подмена значения атрибута `src` элемента `img.lightbox__image`.
+// - Реализация делегирования на галерее `ul.js-gallery` и получение `url` большого изображения.
 const itemGalleryOpenHandler = ({ target, currentTarget }) => {
   if (target.nodeName !== 'IMG') {
     return;
   };
-
   console.log(target);
   console.log(currentTarget);
   
   event.preventDefault();
-  window.addEventListener('keydown', itemGalleryEscHandler);
+  window.addEventListener('keydown', itemGalleryEscHandler);  
   window.addEventListener('keydown', scrollGalleryHandler);
 
-  container.classList.add('is-open');
-  image.src = target.dataset.source;
+  container.classList.add('is-open');        // - Открытие модального окна по клику на элементе галереи.
+  image.src = target.dataset.source;          // - Подмена значения атрибута `src` элемента `img.lightbox__image`.
   image.alt = target.alt;
-  
   console.log(image.alt);
 };
 
-// - Закрытие модального окна по клику на кнопку
-//   `button[data-action="close-lightbox"]`.
-// - Очистка значения атрибута `src` элемента `img.lightbox__image`. Это необходимо
-//   для того, чтобы при следующем открытии модального окна, пока грузится
-//   изображение, мы не видели предыдущее.
 const itemGalleryCloseHandler = () => {
   window.removeEventListener('keydown', scrollGalleryHandler);
-
-  container.classList.remove('is-open');
-  image.src = "";
+  
+  container.classList.remove('is-open');     // - Закрытие модального окна по клику на кнопку `button[data-action="close-lightbox"]`.
+  image.src = "";                         // - Очистка значения атрибута `src` элемента `img.lightbox__image`.
   image.alt = "";
 };
 
 list.addEventListener('click', itemGalleryOpenHandler);
-
 closeBtn.addEventListener('click', itemGalleryCloseHandler);
 overlay.addEventListener('click', itemGalleryCloseHandler);
